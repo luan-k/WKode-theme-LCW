716bf01382c18aabb73db12f7351d06719127488
luan-k <luankds@gmail.com>
2023-06-13 19:20:45 -0300

    im so done with this block... the work is done

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,12 +3,14 @@ import "../styles/main.scss";
 /* import "./base/scroll-entrance"; */
 
 import "./blocks/slider";
+import "./blocks/new-bikes-block";
 
 import "./header/navbar-line";
 import "./header/add-btn-and-divider";
 import "./header/megamenu-logic";
 
 import Search from "./components/Search";
+import jQuery from "jquery";
 
 var search = new Search();
 window.addEventListener("load", () => {
@@ -76,68 +78,3 @@ window.addEventListener("load", () => {
     };
   }
 });
-
-const state = {};
-const carouselItems = document.querySelectorAll(".wkode-carousel__item");
-const elems = Array.from(carouselItems);
-const prevBtn = document.querySelector(".wkode-carousel__arrow_prev");
-const nextBtn = document.querySelector(".wkode-carousel__arrow_next");
-
-prevBtn.addEventListener("click", function () {
-  const current = elems.find((elem) => elem.dataset.pos == 0);
-  const prev = elems.find((elem) => elem.dataset.pos == -1);
-
-  if (!prev) {
-    return;
-  }
-
-  update(prev);
-});
-
-nextBtn.addEventListener("click", function () {
-  const current = elems.find((elem) => elem.dataset.pos == 0);
-  const next = elems.find((elem) => elem.dataset.pos == 1);
-
-  if (!next) {
-    return;
-  }
-
-  update(next);
-});
-
-carouselItems.forEach((item) => {
-  item.addEventListener("click", function (event) {
-    const newActive = this;
-    const isItem = newActive.classList.contains("wkode-carousel__item");
-
-    if (
-      !isItem ||
-      newActive.classList.contains("wkode-carousel__item_active")
-    ) {
-      return;
-    }
-
-    update(newActive);
-  });
-});
-
-const update = function (newActive) {
-  const newActivePos = parseInt(newActive.dataset.pos);
-
-  elems.forEach((item) => {
-    const itemPos = parseInt(item.dataset.pos);
-    item.dataset.pos = getPos(itemPos, newActivePos);
-  });
-
-  newActive.classList.add("wkode-carousel__item_active");
-};
-
-const getPos = function (current, active) {
-  const diff = current - active;
-
-  if (Math.abs(diff) > 3) {
-    return -current;
-  }
-
-  return diff;
-};
--- template-parts/blocks/posts/wkode-new-bikes.php
+++ template-parts/blocks/posts/wkode-new-bikes.php
@@ -1,11 +1,25 @@
 <?php 
 
+$posts = get_field( 'new_bikes_posts' );
+$final_posts;
+var_dump($posts);
+
 $standart_posts = new WP_Query([
     'post_type' => 'motos-novas',
-    'orderby' => 'title',
+    'orderby' => 'date',
     'order' => 'ASC',
-    'posts_per_page' => 36,
+    'posts_per_page' => 7,
 ]);
+
+echo '<br><br><br><br><br>';
+var_dump($standart_posts);
+echo '<br><br><br><br><br>';
+if(count($posts) > 6){
+    $final_posts = 1;
+}else{
+    $final_posts = 0;
+}
+var_dump($posts);
 
 ?>
 
@@ -13,79 +27,112 @@ $standart_posts = new WP_Query([
 <section class="wkode-new-bikes__section">
         <h2 class="wkode-new-bikes__title title ">Modelos em destaque</h2>
         <div class="wkode-new-bikes__category category-filter-container-wrapper ">
-            <div class="category-filter">
-                <div class="category category--current">Todas</div>
-                <div class="category">Street</div>
-                <div class="category">Adventure</div>
-                <div class="category">Off Road</div>
-                <div class="category">Sport</div>
-                <div class="category">Touring</div>
-            </div>
+            <?php
+            //sorry... no time
+            //<div class="category-filter">
+            //    <div class="category category--current">Todas</div>
+            //    <div class="category">Street</div>
+            //    <div class="category">Adventure</div>
+            //    <div class="category">Off Road</div>
+            //    <div class="category">Sport</div>
+            //    <div class="category">Touring</div>
+            //</div>
+            ?>
             <div class="wkode-new-bikes__carousel">
-            <?php 
-            while($standart_posts->have_posts()){
-                $standart_posts->the_post();
-                
-                 // Get the value of the custom field for the current post
-                $custom_field_value = get_field('wkode_motorcycles_post_colors', get_the_ID());
-                    ?>
-                        <div class="wkode-new-bikes__card ">
-                            <h3 class="wkode-new-bikes__card-title ">
-                                <a href="<?php the_permalink(); ?>">
-                                    <?php echo wp_trim_words( the_title() , 15); ?>
-                                </a>
-                            </h3>
-                            <a href="<?php the_permalink(); ?>">
-                                <!-- <img class="wkode-new-bikes__card-img active-color-image" src="<?php if(has_post_thumbnail()){ the_post_thumbnail_url('full'); } else {/*  echo get_theme_file_uri('/images/standard.png'); */ }  ?>" alt="" srcset=""> -->
-                                <?php
-                                foreach ($custom_field_value as $index => $field) {
-                                    $postImg = $field['wkode_motorcycles_post_img'];
-                                    if($index == 0){ ?>
-                                        <img class="wkode-new-bikes__card-img active-color-image" src="<?php if(has_post_thumbnail()){ the_post_thumbnail_url('full'); } else {/*  echo get_theme_file_uri('/images/standard.png'); */ }  ?>" alt="" srcset=""> <?php
-                                    }else{  ?>
-                                        <img class="wkode-new-bikes__card-img" src="<?php echo $postImg; ?>" alt="" srcset=""> <?php
-                                    }
-                                
+                <div class="carousel-container">
+                    <div class="carousel-wrapper">
+                        <div class="carousel">
+                            <?php 
+                            if($final_posts){
+                                foreach ($posts as $post) {
+                                    setup_postdata($post);
+                                    ?>
+                                    <div class="wkode-new-bikes__card carousel-image">
+                                        <h3 class="wkode-new-bikes__card-title ">
+                                            <a href="<?php echo get_permalink($post->ID); ?>">
+                                                <?php echo wp_trim_words(get_the_title($post->ID), 15); ?>
+                                            </a>
+                                        </h3>
+                                        <a href="<?php echo get_permalink($post->ID); ?>">
+                                            <?php
+                                            $custom_field_value = get_field('wkode_motorcycles_post_colors', $post->ID);
+                                            if ($custom_field_value) {
+                                                foreach ($custom_field_value as $index => $field) {
+                                                    $postImg = $field['wkode_motorcycles_post_img'];
+                                                    if ($index == 0) {
+                                                        ?>
+                                                        <img class="wkode-new-bikes__card-img active-color-image" src="<?php echo has_post_thumbnail($post->ID) ? get_the_post_thumbnail_url($post->ID, 'full') : get_theme_file_uri('/images/standard.png'); ?>" alt="" srcset="">
+                                                        <?php
+                                                    } else {
+                                                        ?>
+                                                        <img class="wkode-new-bikes__card-img" src="<?php echo $postImg; ?>" alt="" srcset="">
+                                                        <?php
+                                                    }
+                                                }
+                                            }
+                                            ?>
+                                        </a>
+                                        <div class="wkode-new-bikes__card-colors text-black">
+                                            <?php
+                                            if ($custom_field_value) {
+                                                foreach ($custom_field_value as $index => $field) {
+                                                    $postColor = $field['wkode_motorcycles_post_color'];
+                                                    $biOrTri = $field['wkode_motorcycles_bicolor_ou_tricolor'];
+                                                    $secondColor = $field['wkode_motorcycles_post_color_two'];
+                                                    $thirdColor = $field['wkode_motorcycles_post_color_three'];
+            
+                                                    if ($biOrTri == 'bicolor') {
+                                                        $biOrTriClass = "wkode-new-bikes__card-color--bicolor";
+                                                    } elseif ($biOrTri == 'tricolor') {
+                                                        $biOrTriClass = "wkode-new-bikes__card-color--tricolor";
+                                                    } else {
+                                                        $biOrTriClass = "wkode-new-bikes__card-color--unique";
+                                                    }
+                                                    if ($index == 0) {
+                                                        $active_color = "active-color";
+                                                    } else {
+                                                        $active_color = "";
+                                                    }
+                                                    ?>
+                                                    <span class="wkode-new-bikes__card-color <?php echo $active_color ?>">
+                                                        <span class="<?php echo $biOrTriClass ?>" style="background-color: <?php echo $postColor; ?>"></span>
+                                                        <?php
+                                                        if ($biOrTri == 'bicolor') {
+                                                            ?>
+                                                            <span class="<?php echo $biOrTriClass ?>" style="background-color: <?php echo $secondColor; ?>"></span>
+                                                            <?php
+                                                        }
+                                                        if ($biOrTri == 'tricolor') {
+                                                            ?>
+                                                            <span class="<?php echo $biOrTriClass ?>" style="background-color: <?php echo $secondColor; ?>"></span>
+                                                            <span class="<?php echo $biOrTriClass ?>" style="background-color: <?php echo $thirdColor; ?>"></span>
+                                                            <?php
+                                                        }
+                                                        ?>
+                                                    </span>
+                                                    <?php
+                                                }
+                                            }
+                                            ?>
+                                        </div>
+                                    </div>
+                                    <?php
                                 }
-                                ?>
-                            </a>
-                            <div class="wkode-new-bikes__card-colors text-black">
-                                <?php
-                                foreach ($custom_field_value as $index => $field) {
-                                    $postColor = $field['wkode_motorcycles_post_color'];
-                                    $biOrTri = $field['wkode_motorcycles_bicolor_ou_tricolor'];
-                                    $secondColor = $field['wkode_motorcycles_post_color_two'];
-                                    $thirdColor = $field['wkode_motorcycles_post_color_three'];
+                                wp_reset_postdata();
+                            }else{
+                                while($standart_posts->have_posts()){
+                                    $standart_posts->the_post();
                                     
-                                    if($biOrTri == 'bicolor'){
-                                        $biOrTriClass = "wkode-new-bikes__card-color--bicolor";
-                                    }elseif($biOrTri == 'tricolor'){
-                                        $biOrTriClass = "wkode-new-bikes__card-color--tricolor";
-                                    }else{
-                                        $biOrTriClass = "wkode-new-bikes__card-color--unique";
-                                    }
-                                    if($index == 0){ 
-                                        $active_color = "active-color";
-                                    }else{ 
-                                        $active_color = "";
-                                    } ?>
-                                    <span class="wkode-new-bikes__card-color <?php echo $active_color ?>">
-                                        <span class="<?php echo $biOrTriClass ?>" style="background-color: <?php echo $postColor; ?>"></span>
-                                        <?php
-                                        if($biOrTri == 'bicolor'){ ?>
-                                            <span class="<?php echo $biOrTriClass ?>" style="background-color: <?php echo $secondColor; ?>"></span><?php 
-                                        }if($biOrTri == 'tricolor'){?>
-                                            <span class="<?php echo $biOrTriClass ?>" style="background-color: <?php echo $secondColor; ?>"></span>
-                                            <span class="<?php echo $biOrTriClass ?>" style="background-color: <?php echo $thirdColor; ?>"></span><?php 
-                                        }
-                                        ?>
-                                    </span> <?php
-                                }
-                                ?>
-                            </div>
+                                    get_template_part('./template-parts/cards/new-bikes-carousel');
+                                    
+                                } wp_reset_postdata();
+                            }?>
                         </div>
-                <?php
-            } ?>
+                    </div>
+                    
+                    <button class="arrow-button left-arrow" style="background-image: url(<?php echo get_theme_file_uri('assets/img/svg/new-carrousel-left.svg'); ?>);"></button>
+                    <button class="arrow-button right-arrow" style="background-image: url(<?php echo get_theme_file_uri('assets/img/svg/new-carrousel-right.svg'); ?>);"></button>
+                </div>
             </div>
 
         </div>
