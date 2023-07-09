b7966fb4c6a39369e9f24928ede18e35398a9c0f
Luan Kraisch <luankds@gmail.com>
2023-07-09 12:09:10 -0300

    consorcio

--- assets/scripts/pages/single-modal-logic.js
+++ assets/scripts/pages/single-modal-logic.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Get the button that opens the modal
   let openModalBtn = document.getElementById("openModalBtn");
+  let openModalBtns = document.querySelectorAll(".openModalBtn");
+  console.log(openModalBtns);
 
   if (modal) {
     // Get the <span> element that closes the modal
@@ -36,6 +38,65 @@ document.addEventListener("DOMContentLoaded", function () {
       // Event listener for clicks on the body to remove modal function
       document.body.addEventListener("click", function (event) {
         if (event.target !== openModalBtn && !modal.contains(event.target)) {
+          modal.classList.remove("modal-active");
+          document.body.classList.remove("modal-open");
+        }
+      });
+    }
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the modal element
+  let modal = document.getElementById("myModalConsorcio");
+
+  // Get the button that opens the modal
+  let openModalBtns = document.querySelectorAll(".openModalBtnConsorcio");
+
+  if (modal) {
+    // Get the <span> element that closes the modal
+    let closeBtn = modal.querySelector(".closeConsorcio");
+
+    if (openModalBtns.length > 0 && closeBtn) {
+      // Function to toggle the modal and body classes
+      function toggleModal() {
+        modal.classList.toggle("modal-active");
+        document.body.classList.toggle("modal-open");
+      }
+
+      // Event listener for each open modal button
+      openModalBtns.forEach(function (btn) {
+        btn.addEventListener("click", function (event) {
+          event.preventDefault(); // Prevent the default link behavior
+          toggleModal();
+        });
+      });
+
+      // Event listener for the close button
+      closeBtn.addEventListener("click", toggleModal);
+
+      // Event listener for clicks outside the modal to close it
+      window.addEventListener("click", function (event) {
+        if (event.target === modal) {
+          toggleModal();
+        }
+      });
+
+      // Event listener for clicks on the body to remove modal function
+      document.body.addEventListener("click", function (event) {
+        let isButtonClick = false;
+
+        for (let i = 0; i < openModalBtns.length; i++) {
+          if (
+            event.target === openModalBtns[i] ||
+            openModalBtns[i].contains(event.target)
+          ) {
+            isButtonClick = true;
+            break;
+          }
+        }
+
+        if (!isButtonClick && !modal.contains(event.target)) {
           modal.classList.remove("modal-active");
           document.body.classList.remove("modal-open");
         }
---
+++ page-consorcio.php
@@ -0,0 +1,100 @@
+<?php get_header(); 
+$consorcioRepeater           = get_field('consorcio_repeater');
+
+?>
+
+<div id="primary" class="wkode-single-page-template wkode-single-page-template--consorcio content-area">
+    <main id="main" class="wkode-single-page-template__main site-main" role="main">
+
+        <?php while (have_posts()) : the_post(); ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class('wkode-single-page-template__article'); ?>>
+
+                <div class="wkode-single-page-template__body py-60">
+                    <header class="container mb-28">
+                        <h1 class="text-6xl font-rubik font-extrabold text-left uppercase"><?php the_title(); ?></h1>
+                    </header>
+                    <div class="wkode-single-page-template__content entry-content"><!--  -->
+
+                        <?php 
+
+                        foreach ($consorcioRepeater as $item) {
+                            $consorcioGroup = $item['consorcio_group'];
+                            $consorcioMoto = $consorcioGroup['consorcio_moto'];
+                            $consorcioRepeaterParcelas = $consorcioGroup['consorcio_repeater_parcelas'];
+
+                            // Accessing values for each item
+                            $title = $consorcioMoto->post_title;
+                            $image = get_the_post_thumbnail_url($consorcioMoto->ID);
+
+                            $custom_field_value = get_field('wkode_motorcycles_post_colors', $consorcioMoto->ID);
+                            foreach ($custom_field_value as $index => $field) {
+                                $postImg = $field['wkode_motorcycles_post_img'];
+                                if($index == 0){
+                                    $image = $postImg;
+                                }
+                            }
+                        
+                            ?>
+
+                            <div class="wkode-used-bikes__card wkode-used-bikes__card--consorcio">
+                                <a class="wkode-used-bikes__card-link" >
+                                    <img class="wkode-used-bikes__card-img" src="<?php echo $image; ?>" alt="" srcset=""> 
+                                </a>
+                                <div class="wkode-used-bikes__card-body">
+                                    <h3 class="wkode-used-bikes__card-title ">
+                                        <a >
+                                            <?php echo wp_trim_words( $title , 15); ?>
+                                        </a>
+                                    </h3>
+                                    <div class="wkode-used-bikes__card-info">
+                                        <?php
+                                        // Iterating over consorcio_repeater_parcelas
+                                        foreach ($consorcioRepeaterParcelas as $parcela) {
+                                            $consorcioGroupParcelas = $parcela['consorcio_group_parcelas'];
+                                            $consorcioQuantity = $consorcioGroupParcelas['consorcio_quantity'];
+                                            $consorcioPrice = $consorcioGroupParcelas['consorcio_price']; ?>
+                                            <div class="wkode-used-bike__card-prices"> 
+                                                <h5 class="wkode-used-bike__card-prices-quantity">
+                                                    <?php echo $consorcioQuantity; ?>X
+                                                </h5>
+                                                <h5 class="wkode-used-bike__card-prices-price">
+                                                    R$ <?php echo $consorcioPrice; ?>
+                                                </h5>
+                                            </div>
+                                            <?php
+                                        }
+                                        ?>
+                                    </div>
+                                </div>
+                                <div class="wkode-used-bikes__card-footer ">
+                                    <div class="wkode-used-bikes__card-footer-btn w-full ">
+                                        <a href="<?php  ?>" class="wkode-btn wkode-btn--outline-red w-full text-center openModalBtnConsorcio">
+                                            Solicitar cotação
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+
+                        <?php 
+                        }
+                        ?>
+                    </div>
+                </div>
+
+
+            </article>
+        <?php endwhile; ?>
+
+    </main>
+
+    <div class="wkode-single-used-bikes-template__form modal" id="myModalConsorcio">
+        <h2 class="wkode-single-used-bikes-template__form-title">
+            <span class="closeConsorcio">
+                <img class="wkode-single-used-bikes__img" src="<?php echo get_theme_file_uri('/assets/img/svg/white-x-thick.svg'); ?>" alt="" srcset="">
+            </span>
+        </h2>
+        <?php echo do_shortcode( '[wpforms id="308" title="false"]' ); ?>
+    </div>
+</div>
+
+<?php get_footer(); ?>
