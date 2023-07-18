86d94e0c9ac50fb63da45e0542e7625eff985851
Luan Kraisch <luankds@gmail.com>
2023-07-18 14:33:50 -0300

    stopping author from discord

--- functions.php
+++ functions.php
@@ -148,5 +148,10 @@ class Megamenu_Walker extends Walker_Nav_Menu {
     }
 }
 
-
+add_filter( 'oembed_response_data', 'disable_embeds_filter_oembed_response_data_' );
+function disable_embeds_filter_oembed_response_data_( $data ) {
+    unset($data['author_url']);
+    unset($data['author_name']);
+    return $data;
+}
 ?>
